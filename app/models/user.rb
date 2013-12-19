a77fdf54447307fc534f1c24f27dab071379463f
Mateusz Czajka <mateusz.czajka@netguru.pl>
2013-12-20 00:01:48 +0100

    sign in/out in menu

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :role
   # attr_accessible :title, :body
 
   ROLES = ['cook', 'worker'].freeze
