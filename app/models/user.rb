9aed93117a6296375c67a24d12ee920fc08c5f96
Mateusz Czajka <mateusz.czajka@netguru.pl>
2013-12-21 00:44:41 +0100

    remove role from users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :role, :type
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :type
   # attr_accessible :title, :body
 
   WAITER = 'Waiter'.freeze
   COOK = 'Cook'.freeze
-  ROLES = [WAITER, COOK].freeze
+  TYPES = [WAITER, COOK].freeze
 
   def worker?
-    ROLES.include?(self.type) ? true : false
+    TYPES.include?(self.type) ? true : false
   end
 
   def waiter?
