dc753e7bf709d302206aa682d92bb9890e9a69ce
Mateusz Czajka <mateusz.czajka@netguru.pl>
2013-12-21 00:39:01 +0100

    use decent exposure in spots

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -3,61 +3,39 @@ class SpotsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :is_worker?, except: [:index, :show]
 
+  expose :spot
+  expose :spots
+
   def index
-    @spots = Spot.all
-    respond_to do |format|
-      format.html
-    end
   end
 
   def show
-    @spot = Spot.find(params[:id])
-    respond_to do |format|
-      format.html
-    end
   end
 
   def new
-    @spot = Spot.new
-    respond_to do |format|
-      format.html
-    end
   end
 
   def edit
-    @spot = Spot.find(params[:id])
   end
 
   def create
-    @spot = Spot.new(params[:spot])
-
-    respond_to do |format|
-      if @spot.save
-        format.html { redirect_to @spot, notice: 'Spot was successfully created.' }
-      else
-        format.html { render action: "new" }
-      end
+    if spot.save
+      redirect_to spot, notice: 'Spot was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
   def update
-    @spot = Spot.find(params[:id])
-
-    respond_to do |format|
-      if @spot.update_attributes(params[:spot])
-        format.html { redirect_to @spot, notice: 'Spot was successfully updated.' }
-      else
-        format.html { render action: "edit" }
-      end
+    if spot.save
+      redirect_to spot, notice: 'Spot was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
   def destroy
-    @spot = Spot.find(params[:id])
-    @spot.destroy
-
-    respond_to do |format|
-      format.html { redirect_to spots_url }
-    end
+    spot.destroy
   end
+
 end
