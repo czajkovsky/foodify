f7d25d19deba3b44c1effe3d1f6ae084f423c200
Mateusz Czajka <mateusz.czajka@netguru.pl>
2013-12-21 14:01:41 +0100

    show spots menu for proper user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def current_worker?
+    current_user.worker?
+  end
+
+  def current_client?
+    !current_user.worker?
+  end
+
+  def current_cook?
+    current_user.cook?
+  end
+
+  def current_waiter?
+    current_user.waiter?
+  end
+
 end
