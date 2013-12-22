62002770ad58c08116294aa431c5f3881e3b3b30
Mateusz Czajka <mateusz.czajka@netguru.pl>
2013-12-22 19:16:48 +0100

    further dashboard refactor

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ActiveRecord::Base
   belongs_to :spot
   attr_accessible :amount, :client_id, :cook_id, :state, :waiter_id, :spot_id
+
+  def ready_to_cook?
+    self.state == 'pending'
+  end
+
 end
