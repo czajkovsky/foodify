00deb58654eaeb55bddf1fef30c5264a1ce638ce
Mateusz Czajka <mateusz.czajka@netguru.pl>
2013-12-22 14:07:12 +0100

    you can't have more than 1 open order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,20 +10,30 @@ class OrdersController < ApplicationController
   end
 
   def new
-    new_order = Order.new(
-      client_id: spot.client_id,
-      waiter_id: spot.waiter_id,
-      spot_id: spot.id,
-      amount: 0,
-      state: 'new'
-      )
-    new_order.save
-    redirect_to spot_order_path(spot, new_order)
+    if can_order?(spot.client_id)
+      new_order = Order.new(
+        client_id: spot.client_id,
+        waiter_id: spot.waiter_id,
+        spot_id: spot.id,
+        amount: 0,
+        state: 'new'
+        )
+      new_order.save
+      redirect_to spot_order_path(spot, new_order)
+    else
+      redirect_to spots_path, notice: 'Sorry, you have some new (not sent to kitchen) orders.'
+    end
   end
 
   def destroy
     order.destroy
     redirect_to spot_path(spot)
+  end
+
+private
+
+  def can_order? client
+    Order.where(client_id: client, state: 'new').count == 0
   end
 
 end
